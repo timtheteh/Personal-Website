9f5bcca1d8deb1ef8ddbe6d8b8ccb2f52f37074d
timtheteh <76463517+timtheteh@users.noreply.github.com>
2025-12-27 17:23:31 +0800

    sticky top bar

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,6 +1,6 @@
 export default function TopBar() {
   return (
-    <header className="w-full h-14 tablet:h-16 desktop:h-16 bg-background border-b border-gray-200 dark:border-gray-800">
+    <header className="sticky top-0 z-50 w-full h-14 tablet:h-16 desktop:h-16 bg-background border-b border-gray-200 dark:border-gray-800">
       <div className="container mx-auto px-4 h-full flex items-center">
         {/* Add your top bar content here */}
         <nav className="w-full">
