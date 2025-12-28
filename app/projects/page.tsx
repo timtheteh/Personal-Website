e635ab9aaca7596545672737a9691c2bcd85efa8
timtheteh <76463517+timtheteh@users.noreply.github.com>
2025-12-28 20:33:13 +0800

    removed redundant px

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,7 @@
 export default function Projects() {
   return (
     <main className="min-h-screen py-12 tablet:py-16 desktop:py-20">
-      <div className="max-w-[1200px] border-2 border-green-500 mx-auto px-4 tablet:px-6 desktop:px-8">
+      <div className="max-w-[1200px] border-2 border-green-500 mx-auto">
         {/* Projects content will go here */}
       </div>
     </main>
