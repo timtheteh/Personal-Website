ed38baca67b36a3aed8a5f727da10e5af7a5b5f4
timtheteh <76463517+timtheteh@users.noreply.github.com>
2026-01-03 17:57:58 +0800

    update github social link

--- content/socialLinks.ts
+++ content/socialLinks.ts
@@ -1,5 +1,5 @@
 export const socialLinks = {
-  github: 'https://github.com/timtheteh?tab=repositories',
+  github: 'https://github.com/timtheteh',
   linkedin: 'https://www.linkedin.com/in/timothy-teh/',
   instagram: 'https://instagram.com', // Update this when you have your Instagram URL
 };
